aa32da15dabae39d3bbd4c076a10e3a941245720
Jerosn <Jerosn@DESKTOP-PBDVQLC>
2017-03-09 21:25:52 -0600

    Hoja de trabajo No. 3 Nombre: Jerson Eduardo Villatoro Nova Carnet: 201442819

--- src/org/compi2/codigo3d/parser/ControlC3D.java
+++ src/org/compi2/codigo3d/parser/ControlC3D.java
@@ -8,6 +8,7 @@ public class ControlC3D {
     
     private static int temp = 0;
     private static String c3d = "";
+    private static int etq = 0;
     
     /**
      * Reinicia las variables estáticas relacionadas con la generación del
@@ -16,6 +17,7 @@ public class ControlC3D {
     public static void reiniciar(){
         temp = 0;
         c3d = "";
+        etq = 0;
     }
     
     /**
@@ -24,6 +26,14 @@ public class ControlC3D {
      */
     public static String generaTemp(){
         return "t$"+temp++;
+    }
+    
+    /**
+     * Genera la siguiente etiqueta
+     * @return  La siguiente etiqueta (L#)
+     */
+    public static String generarETQ(){
+        return "L" + etq++;
     }
     
     /**
